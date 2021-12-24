cb82890aa866535f56cc45cf1548b1bf93dba174
manojpujari40 <50852168+manojpujari40@users.noreply.github.com>
2021-12-24 20:45:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function login(){
-    Player1_name = document.getElementById("login_player1").value;
-    Player2_name = document.getElementById("login_player2").value;
-    localStorage.setItem("Player1_name", Player1_name);
-    localStorage.setItem("Player2_name", Player2_name);
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
     window.location = "quiz_game_page.html";
 }
